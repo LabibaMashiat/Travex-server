fb9f139e25bddad33136fe653280396d214d17bc
LabibaMashiat20 <labibamashiatmahi@gmail.com>
2022-11-11 01:46:41 +0600

    mongodb set-up

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app=express();
 const port=process.env.PORT || 5000;
 
 //middle wares
+//travexUser
+//tY4b6HURmjDIQexs
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_USER),
+console.log(process.env.DB_PASSWORD)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ksontsm.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/',(req,res)=>{
     res.send('TraveX Server running')
