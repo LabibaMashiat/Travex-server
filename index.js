70ee0246e46044f06966ea32008dde315337dddd
LabibaMashiat20 <labibamashiatmahi@gmail.com>
2022-11-11 10:48:45 +0600

    getServices added

--- index.js
+++ index.js
@@ -17,11 +17,25 @@ console.log(process.env.DB_PASSWORD)
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ksontsm.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+    try{
+      const serviceCollection = client.db('traveX').collection('services');
+
+      app.get('/services',async(req,res)=>{
+        const query={}
+        const cursor = serviceCollection.find();
+        const services=await cursor.toArray();
+        res.send(services);
+      })
+
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 
 app.get('/',(req,res)=>{
